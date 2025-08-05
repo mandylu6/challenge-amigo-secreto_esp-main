b8ffaca6afa8b08ffcd414a9dbd6f14e299ee543
Amanda Pareja <manddy.6@gmail.com>
2025-08-05 19:20:52 -0400

    primera modificacion

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+let listaAmigos = document.getElementById('amigo');
+let resultado = document.getElementById('resultado');
+let input = document.getElementById('amigo')
+
+function agregarAmigo(){
+    let nombre=input.value();
+    //guardar el array
+    if (nombre!=''){
+        nombres.push(nombre);
+        generarLista();
+        input.value='';
+    }
+}
+function generarLista(){
+    
+}
